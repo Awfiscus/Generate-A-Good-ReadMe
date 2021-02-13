9b6e1f7b644ab387443b97645b1239d007d2b2d2
Drew Weston <awfiscus@comcast.net>
2021-02-12 17:14:41 -0700

    uploaded base files and formated base questions

---
+++ index.js
@@ -0,0 +1,24 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        message: "What is the name of your project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "What is a description of the project?",
+        name: "description"
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
